e7e5710cf65e1046cd16f1d10df1251328f5a292
Pavan Kumar <sonthoshkumar.n@gmail.com>
2026-05-22 09:42:23 +0530

    Initial commit of displaying hello.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <header className="app-header">
+        <h1>Hello Smart Loan</h1>
+        <p>Welcome to the Smart Loan Application</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
